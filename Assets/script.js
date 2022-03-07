0df1bbf3a32540777f01d4aad5e33a6abdb1c170
Laurel Thorburn <laurelthorburn@gmail.com>
2022-03-06 18:16:55 -0700

    Initial commit

---
+++ Assets/script.js
@@ -0,0 +1,168 @@
+$(document).ready(
+    console.log('ready!')
+);
+
+var inputOne;
+
+//Setting current time as a global function
+var currentTime = moment();
+
+function updateClock() {
+    $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
+}
+setInterval(updateClock, 1000);
+
+//Console logging each data-time
+
+
+var currentHour = Number(currentTime.format('H'));
+console.log(typeof(currentHour)); //works, says it's a string so comparing number to string, used number() to convert from string to # sickkk
+
+//time block
+if(currentHour === 9){
+    $('#0').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 9){
+    $('#0').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 10){
+    $('#1').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 10){
+    $('#1').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 11){
+    $('#2').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 11){
+    $('#2').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 12){
+    $('#3').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 12){
+    $('#3').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 13){
+    $('#4').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 13){
+    $('#4').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 14){
+    $('#5').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 14){
+    $('#5').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 15){
+    $('#6').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 15){
+    $('#6').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 16){
+    $('#7').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 16){
+    $('#7').css("background-color", "#77dd77"); //confirmed css works
+};
+if(currentHour === 17){
+    $('#8').css("background-color", "#ff6961"); //confirmed css works
+} else if(currentHour < 17){
+    $('#8').css("background-color", "#77dd77"); //confirmed css works
+};
+
+//button 0 (9am time slot)
+$('#btn0').click(function() {
+    inputZero = document.getElementById("0").value;
+    console.log(inputZero);
+    // // must stringify the object before save
+    localStorage.setItem('9AM', JSON.stringify(inputZero));
+    });
+
+var save9AM = JSON.parse(localStorage.getItem('9AM'));
+console.log(save9AM); // works
+
+document.getElementById("0").value = save9AM;
+
+//button 1 (10am time slot)
+$('#btn1').click(function() {
+    inputOne = document.getElementById("1").value;
+    // // must stringify the object before save
+    localStorage.setItem('10AM', JSON.stringify(inputOne));
+    });
+
+var save10AM = JSON.parse(localStorage.getItem('10AM'));
+
+document.getElementById("1").value = save10AM;
+
+//button 2 (11am time slot)
+$('#btn2').click(function() {
+    inputTwo = document.getElementById("2").value;
+    // // must stringify the object before save
+    localStorage.setItem('11AM', JSON.stringify(inputTwo));
+    });
+
+var save11AM = JSON.parse(localStorage.getItem('11AM'));
+
+document.getElementById("2").value = save11AM;
+
+//button 3 (12pm time slot)
+$('#btn3').click(function() {
+    inputThree = document.getElementById("3").value;
+    // // must stringify the object before save
+    localStorage.setItem('12PM', JSON.stringify(inputThree));
+    });
+
+var save12PM = JSON.parse(localStorage.getItem('12PM'));
+
+document.getElementById("3").value = save12PM;
+
+//button 4 (1pm time slot)
+$('#btn4').click(function() {
+    inputFour = document.getElementById("4").value;
+    // // must stringify the object before save
+    localStorage.setItem('1PM', JSON.stringify(inputFour));
+    });
+
+var save1PM = JSON.parse(localStorage.getItem('1PM'));
+
+document.getElementById("4").value = save1PM;
+
+//button 5 (2pm time slot)
+$('#btn5').click(function() {
+    inputFive = document.getElementById("5").value;
+    // // must stringify the object before save
+    localStorage.setItem('2PM', JSON.stringify(inputFive));
+    });
+
+var save2PM = JSON.parse(localStorage.getItem('2PM'));
+
+document.getElementById("5").value = save2PM;
+
+//button 6 (3pm time slot)
+$('#btn6').click(function() {
+    inputSix = document.getElementById("6").value;
+    // // must stringify the object before save
+    localStorage.setItem('3PM', JSON.stringify(inputSix));
+    });
+
+var save3PM = JSON.parse(localStorage.getItem('3PM'));
+
+document.getElementById("6").value = save3PM;
+
+//button 7 (4pm time slot)
+$('#btn7').click(function() {
+    inputSeven = document.getElementById("7").value;
+    // // must stringify the object before save
+    localStorage.setItem('4PM', JSON.stringify(inputSeven));
+    });
+
+var save4PM = JSON.parse(localStorage.getItem('4PM'));
+
+document.getElementById("7").value = save4PM;
+
+//button 8 (5pm time slot)
+$('#btn8').click(function() {
+    inputEight = document.getElementById("8").value;
+    // // must stringify the object before save
+    localStorage.setItem('5PM', JSON.stringify(inputEight));
+    });
+
+var save5PM = JSON.parse(localStorage.getItem('5PM'));
+
+document.getElementById("8").value = save5PM;
+
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# Switch-Practice
-Playground for practicing switch cases
+# Work-Planner
+
+## Description
+The purpose of this project was to create a daily planner that allows the user to store their agenda in hour time slots.  In addition, the user is able to see the current time with a red background, past time slots in gray, and future time slots in green.  The user is able to save their input upon button click and the data is available after they refresh the page.
+
+This project helped me continue to develop my css and javascript skills.  I was also able to use Moment.js and bootstrap to save time styling and coding.  I enjoyed seeing how much my skills have grown.
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+
+# Website Video
+
+![Screenshot of Laurel Thorburn's Work Planner](Assets/Images/WorkPlanner.gif)
+
+# Links
+
+Github: https://github.com/laurelthorburn/Work-Planner
+
+Deployed Site: https://laurelthorburn.github.io/Work-Planner/
+
+
+# Resources/Credit
+
+* https://stackoverflow.com/questions/62904905/how-do-i-color-code-time-blocks-using-js
+* https://www.sitepoint.com/managing-dates-times-using-moment-js/
+* https://momentjs.com/docs/#/displaying/
+* https://www.w3schools.com/jsref/jsref_number.asp
+* https://stackoverflow.com/questions/22680413/how-to-invert-the-color-of-a-png-image-on-mouseover-which-is-inside-a-tag
+
+
